5c4e1dc13ecbbc8da20d305c98a8748b621829be
kimjieung97 <dickseung23@naver.com>
2023-07-08 22:02:14 +0900

    # 스프링 부트 프로젝트 초기화

---
+++ src/test/java/com/fastboard/fastboardproject/FastboardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastboard.fastboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastboardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
