8f3f300341861d6a15a7ce024b743f4b992fca90
kimjieung97 <dickseung23@naver.com>
2023-07-15 17:07:46 +0900

    # 댓글 게시글 객체 생성

--- src/main/java/com/fastboard/fastboardproject/domain/Article.java
+++ src/main/java/com/fastboard/fastboardproject/domain/Article.java
@@ -1,2 +1,21 @@
-package com.fastboard.fastboardproject.domain;public class Article {
+package com.fastboard.fastboardproject.domain;
+
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    private Long id;
+
+    private String content;
+    private String title;
+
+    private String hashTag;
+
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
 }
