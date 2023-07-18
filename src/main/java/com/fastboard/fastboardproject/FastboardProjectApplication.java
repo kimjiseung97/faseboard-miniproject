44d931509e1245f92bef373e58e3ea02a2cfb2af
kimjieung97 <dickseung23@naver.com>
2023-07-18 23:38:42 +0900

    #10- 의존성 업데이트

--- src/main/java/com/fastboard/fastboardproject/FastboardProjectApplication.java
+++ src/main/java/com/fastboard/fastboardproject/FastboardProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FastboardProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastboardProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FastboardProjectApplication.class, args);
+    }
 
 }
