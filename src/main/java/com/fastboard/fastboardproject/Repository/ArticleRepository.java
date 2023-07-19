f3649b9f4b282fb7084e851e0bc801ae1a864a06
kimjieung97 <dickseung23@naver.com>
2023-07-20 00:52:28 +0900

    # Spring Data Rest 아노테이션 추가
    hal Json 추가
    게시글,댓글 json api를 자동으로 만들게끔 설정

--- src/main/java/com/fastboard/fastboardproject/Repository/ArticleRepository.java
+++ src/main/java/com/fastboard/fastboardproject/Repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package com.fastboard.fastboardproject.Repository;
 
 import com.fastboard.fastboardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
